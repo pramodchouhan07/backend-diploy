28fc1c7cb6baf3b7ab53945e2d627497399fff7d
Pramodchouhan07 <aanjanapramod8@gmail.com>
2026-03-11 10:40:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT
+
+
+app.get('/',(req,res) => {
+    res.send("Hello World!")
+})
+
+app.get("/github",(req,res) => {
+    res.send("pramodchouhan07")
+})
+app.listen(port,() => {
+    console.log(`Example app is listening on port : ${port}`);
+    
+})
